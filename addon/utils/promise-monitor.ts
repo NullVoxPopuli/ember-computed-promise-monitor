f8619111d21396a9c1aba66b541c501ab23296a6
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-10-31 15:20:03 -0400

    Separate the error from the result

--- addon/utils/promise-monitor.ts
+++ addon/utils/promise-monitor.ts
@@ -1,24 +1,25 @@
 import { set } from "@ember/object";
 
 // import { computed } from '@ember/object';
+
+// Disabled for now, but maybe using this would provide a simpler implementation?
+// not sure if people would actually want the proxy-functionality of this
+// implementation
 //
-// // Disabled for now, but maybe using this would provide a simpler implementation?
-// // not sure if people would actually want the proxy-functionality of this
-// // implementation
-// //
-// // Also, In order to use this, I'd need to find a way to catch exceptions
-// // on rejected promises...
+// Also, In order to use this, I'd need to find a way to catch exceptions
+// on rejected promises...
 // import ObjectProxy from '@ember/object/proxy';
 // import PromiseProxyMixin from '@ember/object/promise-proxy-mixin';
-//
+
 // const ObjectPromiseProxy = ObjectProxy.extend(PromiseProxyMixin);
-//
+
 // export default class PromiseMonitor<T> extends ObjectPromiseProxy {
 //   constructor(promise: Promise<T>) {
 //     super({ promise });
 //   }
-//
+
 //   result = computed('content', 'reason', function() {
+//     console.log(this.reason);
 //     return this.reason || this.content;
 //   });
 // }
@@ -27,7 +28,8 @@ export default class PromiseMonitor<T> {
   isRejected = false;
   isFulfilled = false;
   isPending = true;
-  result: T | Error | undefined;
+  result?: T;
+  error?: Error;
 
   private promise!: Promise<T>;
 
@@ -50,12 +52,12 @@ export default class PromiseMonitor<T> {
   private didFulfill(result: T) {
     set(this, 'isPending', false);
     set(this, 'isFulfilled', true);
-    set(this, 'result', result);
+    set(this, 'result', result as any);
   }
 
   private didReject(error: Error) {
     set(this, 'isPending', false);
     set(this, 'isRejected', true);
-    set(this, 'result', error);
+    set(this, 'error', error);
   }
 }
