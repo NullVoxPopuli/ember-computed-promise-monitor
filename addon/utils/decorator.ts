758d84361268b78f075db1d220538342791f9427
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-04-23 09:32:41 -0400

    fix decorator

--- addon/utils/decorator.ts
+++ addon/utils/decorator.ts
@@ -1,30 +1,46 @@
-import PromiseMonitor from './promise-monitor';
+import PromiseMonitor from "./promise-monitor";
 
 // https://tc39.github.io/proposal-decorators/#sec-elementdescriptor-specification-type
-interface ElementDescriptor {
-  descriptor: PropertyDescriptor;
-  initializer?: () => any; // unknown
-  key: string;
-  kind: 'method' | 'field' | 'initializer';
-  placement: 'own' | 'prototype' | 'static';
-  finisher?: (klass: any) => any;
-}
+// interface ElementDescriptor {
+//   descriptor: PropertyDescriptor;
+//   initializer?: () => any; // unknown
+//   key: string;
+//   kind: 'method' | 'field' | 'initializer';
+//   placement: 'own' | 'prototype' | 'static';
+//   finisher?: (klass: any) => any;
+// }
 
-export function monitor<T = any>(desc: ElementDescriptor) {
-  const { descriptor } = desc;
+// stage 1 / new stage 2?
+export function monitor<T = any>(
+  _target: any,
+  _propertyKey: string,
+  descriptor: PropertyDescriptor
+) {
   const { get: oldGet } = descriptor;
 
-  return {
-    ...desc,
-    kind: 'method',
-    descriptor: {
-      ...desc.descriptor,
+  descriptor.get = function() {
+    const promise = oldGet!.apply(this);
 
-      get() {
-        const promise = oldGet!.apply(this);
-
-        return new PromiseMonitor<T>(promise);
-      },
-    },
+    return new PromiseMonitor<T>(promise);
   };
 }
+
+// old stage 2:
+// export function monitor<T = any>(desc: ElementDescriptor) {
+//   const { descriptor } = desc;
+//   const { get: oldGet } = descriptor;
+
+//   return {
+//     ...desc,
+//     kind: 'method',
+//     descriptor: {
+//       ...desc.descriptor,
+
+//       get() {
+//         const promise = oldGet!.apply(this);
+
+//         return new PromiseMonitor<T>(promise);
+//       },
+//     },
+//   };
+// }
